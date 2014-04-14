fc47d3d6c3a959077c89d607979b219ed1de803c
Roberto Gomez <mail@robertogomezjr.com>
2014-04-13 21:17:42 -0700

    Reduced requests to groups of 50 video IDs
    Instead of creating separate requests for each matched video ID, the
    requests have been grouped into segments of 50. (the maximum amount
    allowed per request) This should minimize the network traffic and result
    in faster response times.
    
    Removed the MatchedBookmark object definition since it doesn't fit this
    new model, but may create a similar object for the video ID
    segments.

--- js/main.js
+++ js/main.js
@@ -1,84 +1,11 @@
 var Ybc = (function() {
     var removedVideos = [],     // Bookmarks which refer to removed videos from YouTube
         checkBoxes    = [],     // Checkboxes in each table row which correspond to the removed video
-        responseCount,          // Number of responses received
-        mbArray       = [],     // Array of MatchedBookmark objects
+        videoIds      = [],     // The YouTube video ids for all the matched bookmarks
         tableList,              // The list.js List object for making the table sortable
         options       = { valueNames: ["name", "folder", "date"] }, // Options for the List object
         tableBody     = document.getElementById("table-body"),      // References to HTML elements
         deleteButton  = document.getElementById("delete-button");
-
-    var MatchedBookmark = function(request, node, index) {
-        this.request = request;
-        this.node = node;
-        this.index = index;
-
-        // Store a reference to the constructed object for
-        // calling addToTable() from within sendRequest()
-        var self = this;
-
-        this.addToTable = function() {
-            var table      = document.getElementById("table"),
-                tableRow   = document.createElement("tr"),
-                selectCell = document.createElement("td"),
-                nameCell   = document.createElement("td"),
-                folderCell = document.createElement("td"),
-                dateCell   = document.createElement("td"),
-                checkBox   = document.createElement("input"),
-                link       = document.createElement("a"),
-                date       = new Date(this.node.children[this.index].dateAdded);
-
-            // Display the table if it is hidden
-            if (window.getComputedStyle(table, null).getPropertyValue("display") === "none")
-                table.style.display = "table";
- 
-            // Add the bookmark to the table
-            checkBox.setAttribute("type", "checkbox");
-            // Bind to the click event instead of change to avoid chain-firing from select-all
-            checkBox.addEventListener("click", toggleDeleteButton, false);
-            link.setAttribute("href", this.node.children[this.index].url);
-            link.textContent = this.node.children[this.index].title;
-            // For the List object, indicate the content to sort by
-            link.classList.add("name");
-            folderCell.classList.add("folder");
-            dateCell.classList.add("date");
-            selectCell.appendChild(checkBox);
-            selectCell.classList.add("checkbox-cell");
-            nameCell.appendChild(link);
-            folderCell.textContent = this.node.title;
-            dateCell.textContent = date.toLocaleDateString();
-            tableRow.appendChild(selectCell);
-            tableRow.appendChild(nameCell);
-            tableRow.appendChild(folderCell);
-            tableRow.appendChild(dateCell);
-            tableBody.appendChild(tableRow);
-
-            // Save a reference to the bookmark's checkbox
-            checkBoxes.push(checkBox);
-
-            console.log(this.node.children[this.index].title + " has been removed from YouTube...");
-        };
-
-        this.sendRequest = function() {
-            this.request.execute(function(response) {
-                responseCount++;
-
-                // If there are no results for the requested
-                // video, save a reference to its bookmark
-                if (response.pageInfo.totalResults === 0)
-                    removedVideos.push(self);
-
-                // If this is the last response, add the bookmarks to the table
-                if (responseCount === mbArray.length) {
-                    for (var i=0; i<removedVideos.length; i++)
-                        removedVideos[i].addToTable();
-
-                    // Create the List object once the table is complete
-                    tableList = new List("results", options);
-                }
-            });
-        };
-    };
 
     // Search for bookmarks in the specified node
     // Regex from Marc's comment on Stack Overflow
@@ -87,9 +14,7 @@ var Ybc = (function() {
     var scanNode = function(node) {
         var regex = /^.*(?:youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/,
             match,
-            videoId,
-            request,
-            mb;
+            videoId;
 
         // Iterate over the child nodes of the parent node
         for (var i=0; i<node.children.length; i++) {
@@ -98,14 +23,9 @@ var Ybc = (function() {
                 match = node.children[i].url.match(regex);
                 // Length of 11 is necessary to avoid false positives, since video id's are 11 chars
                 videoId = (match && match[1].length === 11) ? match[1] : null;
-                // If the video id is valid, assemble the request, create a new
-                // matchedBookmark object, and add it to the array
-                if (videoId) {
-                    request = gapi.client.youtube.videos.list({"part": "status", "id": videoId});
-
-                    mb = new MatchedBookmark(request, node, i);
-                    mbArray.push(mb);
-                }
+                // Store the video id if it is valid
+                if (videoId)
+                    videoIds.push(videoId);
             }
         }
     };
@@ -118,6 +38,7 @@ var Ybc = (function() {
             traverseTree(node.children[i]);
     };
 
+    // Start the bookmarks retrieval process
     var getBookmarks = function() {
         // Remove any old table entries
         for (var i=0; i<checkBoxes.length; i++)
@@ -128,15 +49,36 @@ var Ybc = (function() {
         checkBoxes = [];
         responseCount = 0;
 
-        // Get the bookmarks tree and perform the traversal on it
+        // Get the bookmarks tree and traverse it for YouTube bookmarks
         // Need to pass first element of tree since getTree always returns an array
+        // Once the traversal is complete, assemble the requests for the
+        // matched YouTube bookmarks, send them, and check the responses
         chrome.bookmarks.getTree(function(tree) {
+            var videoIdSegments = new Array(Math.ceil(videoIds.length / 50)),
+                requests = new Array(videoIdSegments.length);
+
             traverseTree(tree[0]);
 
-            // When the traversal is completed, send the requests
-            console.log("Traversal completed...");
-            for (var i=0; i<mbArray.length; i++)
-                mbArray[i].sendRequest();
+            // Divide videoIds into segments of 50 since
+            // the API only allows up to 50 videos per request
+            for (var i=0, beg=0, end=50; i<videoIdSegments.length; i++, beg+=50, end+=50)
+                videoIdSegments[i] = videoIds.slice(beg, end);
+
+            // Assemble the requests
+            for (i=0; i<requests.length; i++)
+                requests[i] = gapi.client.youtube.videos.list(
+                    {"part": "id,status", "id": videoIdSegments[i].join()});
+
+            // Send the requests and check the responses
+            for (i=0; i<requests.length; i++) {
+                requests[i].execute(function(response) {
+                    console.log(response);
+                    //if (response.pageInfo.totalResults === videoIds.length)
+                    //    return;      // All videos are ok
+                    //else
+                    //    determine which videos are removed
+                });
+            }
         });
     };
  
